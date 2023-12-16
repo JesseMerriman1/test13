7c8880ecbed6f3f934a1e106262df637f96cb571
JesseMerriman1 <108379513+JesseMerriman1@users.noreply.github.com>
2023-12-16 14:05:07 -0600

    Update main.js

--- client/js/main.js
+++ client/js/main.js
@@ -49,165 +49,166 @@ document.addEventListener('DOMContentLoaded', function () {
             searchRecords();
         });
     }
-
-    // Client management functions
-    function addClient() {
-        const clientData = {
-            name: document.getElementById('client-name').value,
-            phoneNumber: document.getElementById('phone-number').value,
-            address: document.getElementById('address').value,
-            petsName: document.getElementById('pets-name').value
-        };
-
-        fetch('/api/clients', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(clientData)
-        })
-            .then(handleResponse)
-            .then(data => {
-                console.log('Client added:', data);
-                alert('Client added successfully!');
-                closeModal('add-client-modal');
-                searchClients();
-            })
-            .catch(handleError);
-    }
-
-    function searchClients() {
-        const query = document.getElementById('search-query').value;
-        fetch(`/api/clients/search?term=${encodeURIComponent(query)}`)
-            .then(handleResponse)
-            .then(data => {
-                const resultsContainer = document.getElementById('search-results');
-                resultsContainer.innerHTML = data.length === 0 ? '<p>No clients found.</p>' :
-                    data.map(client => `
-                        <div>
-                            Client ID: ${client.client_id}, 
-                            Name: ${client.name}, 
-                            Phone Number: ${client.phoneNumber || 'N/A'}, 
-                            Address: ${client.address || 'N/A'}, 
-                            Pets Name: ${client.petsName || 'N/A'}
-                            <button onclick="deleteClient('${client.client_id}')">Delete</button>
-                        </div>
-                    `).join('');
-                document.getElementById('search-results-modal').style.display = 'block';
-            })
-            .catch(handleError);
-    }
-
-    function deleteClient(clientId) {
-        console.log("Attempting to delete client with ID:", clientId);
-        if (!clientId) {
-            console.error('Client ID is undefined');
-            alert('Error: Client ID is undefined. Cannot delete client.');
-            return;
-        }
-
-        fetch(`/api/clients/${clientId}`, { method: 'DELETE' })
-            .then(handleResponse)
-            .then(() => {
-                alert('Client deleted successfully!');
-                searchClients();
-            })
-            .catch(handleError);
-    }
-
-    // Patient records management functions
-    function addRecord() {
-        const recordData = {
-            patient_id: document.getElementById('patient-id').value,
-            date_of_visit: document.getElementById('date-of-visit').value,
-            notes: document.getElementById('notes').value,
-            treatment_plan: document.getElementById('treatment-plan').value
-        };
-
-        fetch('/api/patient-records', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(recordData)
-        })
-            .then(handleResponse)
-            .then(data => {
-                console.log('Record added:', data);
-                alert('Record added successfully!');
-                closeModal('add-record-modal');
-                searchRecords();
-            })
-            .catch(handleError);
-    }
-
-    function searchRecords() {
-        const query = document.getElementById('search-query').value;
-        fetch(`/api/patient-records/${query}`)
-            .then(handleResponse)
-            .then(data => {
-                const resultsContainer = document.getElementById('search-results');
-                resultsContainer.innerHTML = data.length === 0 ? '<p>No records found.</p>' :
-                    data.map(record => `
-                        <div>
-                            Record ID: ${record.record_id}, 
-                            Patient ID: ${record.patient_id}, 
-                            Date: ${record.date_of_visit}, 
-                            Notes: ${record.notes || 'N/A'}, 
-                            Treatment Plan: ${record.treatment_plan || 'N/A'}
-                            <button onclick="deleteRecord(${record.record_id})">Delete</button>
-                        </div>
-                    `).join('');
-                document.getElementById('search-results-modal').style.display = 'block';
-            })
-            .catch(handleError);
-    }
-
-    function deleteRecord(recordId) {
-        console.log("Attempting to delete record with ID:", recordId);
-        if (!recordId) {
-            console.error('Record ID is undefined');
-            alert('Error: Record ID is undefined. Cannot delete record.');
-            return;
-        }
-
-        fetch(`/api/patient-records/${recordId}`, { method: 'DELETE' })
-            .then(handleResponse)
-            .then(() => {
-                alert('Record deleted successfully!');
-                searchRecords();
-            })
-            .catch(handleError);
-    }
-
-    // Modal control functions
-    function closeModal(modalId = '') {
-        const modal = modalId ? document.getElementById(modalId) : document.querySelector('.modal.show');
-        if (modal) {
-            modal.style.display = 'none';
-        }
-    }
-
-    function openClientForm() {
-        const addClientModal = document.getElementById('add-client-modal');
-        if (addClientModal) {
-            addClientModal.style.display = 'block';
-        }
-    }
-
-    function openRecordForm() {
-        const addRecordModal = document.getElementById('add-record-modal');
-        if (addRecordModal) {
-            addRecordModal.style.display = 'block';
-        }
-    }
-
-    // Response handling
-    function handleResponse(response) {
-        if (!response.ok) {
-            throw new Error('Network response was not ok: ' + response.statusText);
-        }
-        return response.json();
-    }
-
-    function handleError(error) {
-        console.error('Fetch error:', error);
-        alert('An error occurred. Check the console for more details.');
-    }
 });
+
+// Client management functions
+function addClient() {
+    const clientData = {
+        name: document.getElementById('client-name').value,
+        phoneNumber: document.getElementById('phone-number').value,
+        address: document.getElementById('address').value,
+        petsName: document.getElementById('pets-name').value
+    };
+
+    fetch('/api/clients', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(clientData)
+    })
+        .then(handleResponse)
+        .then(data => {
+            console.log('Client added:', data);
+            alert('Client added successfully!');
+            closeModal('add-client-modal');
+            searchClients();
+        })
+        .catch(handleError);
+}
+
+function searchClients() {
+    const query = document.getElementById('search-query').value;
+    fetch(`/api/clients/search?term=${encodeURIComponent(query)}`)
+        .then(handleResponse)
+        .then(data => {
+            const resultsContainer = document.getElementById('search-results');
+            resultsContainer.innerHTML = data.length === 0 ? '<p>No clients found.</p>' :
+                data.map(client => `
+                    <div>
+                        Client ID: ${client.client_id}, 
+                        Name: ${client.name}, 
+                        Phone Number: ${client.phoneNumber || 'N/A'}, 
+                        Address: ${client.address || 'N/A'}, 
+                        Pets Name: ${client.petsName || 'N/A'}
+                        <button onclick="deleteClient('${client.client_id}')">Delete</button>
+                    </div>
+                `).join('');
+            document.getElementById('search-results-modal').style.display = 'block';
+        })
+        .catch(handleError);
+}
+
+function deleteClient(clientId) {
+    console.log("Attempting to delete client with ID:", clientId);
+    if (!clientId) {
+        console.error('Client ID is undefined');
+        alert('Error: Client ID is undefined. Cannot delete client.');
+        return;
+    }
+
+    fetch(`/api/clients/${clientId}`, { method: 'DELETE' })
+        .then(handleResponse)
+        .then(() => {
+            alert('Client deleted successfully!');
+            searchClients();
+        })
+        .catch(handleError);
+}
+
+// Patient records management functions
+function addRecord() {
+    const recordData = {
+        patient_id: document.getElementById('patient-id').value,
+        date_of_visit: document.getElementById('date-of-visit').value,
+        notes: document.getElementById('notes').value,
+        treatment_plan: document.getElementById('treatment-plan').value
+    };
+
+    fetch('/api/patient_records', {  // Use underscore instead of hyphen
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(recordData)
+    })
+    
+        .then(handleResponse)
+        .then(data => {
+            console.log('Record added:', data);
+            alert('Record added successfully!');
+            closeModal('add-record-modal');
+            searchRecords();
+        })
+        .catch(handleError);
+}
+
+function searchRecords() {
+    const query = document.getElementById('search-query').value;
+    fetch(`/api/patient-records/search?term=${encodeURIComponent(query)}`)
+        .then(handleResponse)
+        .then(data => {
+            const resultsContainer = document.getElementById('search-results');
+            resultsContainer.innerHTML = data.length === 0 ? '<p>No records found.</p>' :
+                data.map(record => `
+                    <div>
+                        Record ID: ${record.record_id}, 
+                        Patient ID: ${record.patient_id}, 
+                        Date: ${record.date_of_visit}, 
+                        Notes: ${record.notes || 'N/A'}, 
+                        Treatment Plan: ${record.treatment_plan || 'N/A'}
+                        <button onclick="deleteRecord(${record.record_id})">Delete</button>
+                    </div>
+                `).join('');
+            document.getElementById('search-results-modal').style.display = 'block';
+        })
+        .catch(handleError);
+}
+
+function deleteRecord(recordId) {
+    console.log("Attempting to delete record with ID:", recordId);
+    if (!recordId) {
+        console.error('Record ID is undefined');
+        alert('Error: Record ID is undefined. Cannot delete record.');
+        return;
+    }
+
+    fetch(`/api/patient-records/${recordId}`, { method: 'DELETE' })
+        .then(handleResponse)
+        .then(() => {
+            alert('Record deleted successfully!');
+            searchRecords();
+        })
+        .catch(handleError);
+}
+
+// Modal control functions
+function closeModal(modalId = '') {
+    const modal = modalId ? document.getElementById(modalId) : document.querySelector('.modal.show');
+    if (modal) {
+        modal.style.display = 'none';
+    }
+}
+
+function openClientForm() {
+    const addClientModal = document.getElementById('add-client-modal');
+    if (addClientModal) {
+        addClientModal.style.display = 'block';
+    }
+}
+
+function openRecordForm() {
+    const addRecordModal = document.getElementById('add-record-modal');
+    if (addRecordModal) {
+        addRecordModal.style.display = 'block';
+    }
+}
+
+// Response handling
+function handleResponse(response) {
+    if (!response.ok) {
+        throw new Error('Network response was not ok: ' + response.statusText);
+    }
+    return response.json();
+}
+
+function handleError(error) {
+    console.error('Fetch error:', error);
+    alert('An error occurred. Check the console for more details.');
+}
