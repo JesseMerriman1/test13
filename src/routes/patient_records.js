682b93b813eb5221379ea610b0108a3637631d91
JesseMerriman1 <108379513+JesseMerriman1@users.noreply.github.com>
2023-12-16 13:44:08 -0600

    test

--- src/routes/patient_records.js
+++ src/routes/patient_records.js
@@ -3,21 +3,21 @@ const router = express.Router();
 const PatientRecordsController = require('../controllers/PatientRecordsController');
 
 // Route to search patient records
-router.get('/api/patient-records/search', PatientRecordsController.searchPatientRecords);
+router.get('/patient-records/search', PatientRecordsController.searchPatientRecords);
 
 // Route to get all patient records
-router.get('/api/patient-records', PatientRecordsController.getAllPatientRecords);
+router.get('/patient-records', PatientRecordsController.getAllPatientRecords);
 
 // Route to get a specific patient record by ID
-router.get('/api/patient-records/:id', PatientRecordsController.getPatientRecordById);
+router.get('/patient-records/:id', PatientRecordsController.getPatientRecordById);
 
 // Route to create a new patient record
-router.post('/api/patient-records', PatientRecordsController.createPatientRecord);
+router.post('/patient-records', PatientRecordsController.createPatientRecord);
 
 // Route to update an existing patient record
-router.put('/api/patient-records/:id', PatientRecordsController.updatePatientRecord);
+router.put('/patient-records/:id', PatientRecordsController.updatePatientRecord);
 
 // Route to delete a patient record
-router.delete('/api/patient-records/:id', PatientRecordsController.deletePatientRecord);
+router.delete('/patient-records/:id', PatientRecordsController.deletePatientRecord);
 
 module.exports = router;
